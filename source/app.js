98ddff92b1c9505b75861678199909aa6941f187
Naresh K <75817967+Nareshk-web@users.noreply.github.com>
2021-03-18 12:19:59 +0530

    Delete source directory

--- source/app.js
+++
@@ -1,8 +0,0 @@
-const hamburgerButton=document.getElementById('hamburger')
-const navLiLst=document.getElementById('nav-list')
-
-function toggleButton(){
-    navLiLst.classList.toggle('show')
-}
-
-hamburgerButton.addEventListener('click',toggleButton)
